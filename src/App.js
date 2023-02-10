c77aba7360341ba535e1c4dc9dd7ff8b8a394bdc
Iryna B <imbigdash@gmail.com>
2023-02-10 23:37:42 +0000

    added GitHub Repo URL
    and hosted on Netlify;

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ export default function App() {
   return (
     <div className="App">
       <h1>Helloy</h1>
+      <footer>
+        <p>This project is creaded by <a href="https://iryna-bigdash.github.io/my-react-weather/" target="_blank">Iryna Bigdash</a>
+      </p>
+      </footer>
     </div>
   );
-  
+
 }
 
