bb35ea8e3ccb33f4cc159cb85a1501092f9e951c
Iryna B <imbigdash@gmail.com>
2023-02-11 15:13:17 +0000

    added css

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Weather.css";
 
+
 export default function Weather(){
 
 return (
 <div className="Weather">
     <form>
         <div className="row">
-            <div className="col-9">
+        <div className="col-9">
         <input type="search" 
         placeholder="Enter a cyty.."
          className="form-conrtol">
@@ -15,8 +16,8 @@ return (
         </div>
         <div className="col-3">
         <input type="submit"
-        value="search"
-        className="btn btn-primary">
+        value="Search"
+        className="btn btn-primary w-100">
         </input>
         </div>
         </div>
@@ -30,11 +31,15 @@ Wensday 07:00
 Mostly Cloudy  
     </li>
   </ul>
-  <div className="row">
+  <div className="row mt-3">
     <div className="col-6">
+        <div className="clearfix">
         <img crs="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-        alt="Sunny">
-        </img> 6°
+        alt="Sunny" className="float-left">
+        </img> 
+
+            <span className="temperature">6</span><span className="unit">°C</span>
+    </div>
     </div>
     <div className="col-6">
         <ul>
